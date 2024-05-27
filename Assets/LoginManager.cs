8542d6f4b3ad74550214e05d2efbfacb815480e0
yarisuli <yarigenco@gmail.com>
2024-05-27 14:05:18 -0300

    verificacion contra funciona

---
+++ Assets/LoginManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoginManager : MonoBehaviour
+{
+    public InputField inputContra;
+    string contra = "hola123";
+
+    public void Verificacion()
+    {
+
+        if (inputContra.text == contra)
+        {
+            Debug.Log("Access granted.");
+        } 
+        else
+        {
+            Debug.Log("Access denied.");
+        }
+    }
+
+    
+}
